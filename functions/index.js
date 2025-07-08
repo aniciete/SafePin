63f30156720572f030b7ff4a371d9df1bd850821
charleslawrenceaniciete <aniciete.charleslawrence@gmail.com>
2025-07-08 14:09:42 +0800

    refactor: Remove unused rate limiting and user listing functions

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,12 @@
 import admin from 'firebase-admin';
-import { rateLimitReport } from './rateLimit.js';
 import { deleteReportImage } from './deleteReportImage.js';
 import { validateReport } from './reportValidation.js';
-import { listUsers } from './listUsers.js';
 
 // Initialize Firebase Admin
 admin.initializeApp();
 
 // Export functions
 export {
-    rateLimitReport,
     deleteReportImage,
     validateReport,
-    listUsers
 };
